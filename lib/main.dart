b722a80f085807888406fd9ca44cf6e531a38784
zainab-frfr <135985226+zainab-frfr@users.noreply.github.com>
2025-02-14 18:44:47 +0500

    tutorial 1 code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tutorial_1/profile_page.dart';
+import 'package:tutorial_1/widgets/custom_appbar.dart';
+import 'package:tutorial_1/widgets/event_tile.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +11,64 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyProfilePage()
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Column(
+            children: [
+              AppBar(
+                shape: CustomAppBarShape(),
+                backgroundColor: Colors.blueGrey,
+              ),
+              Container(
+                height: 56 * 4.0 * 0.65,
+              ),
+              ClipOval(
+                child: Image.asset(
+                  'assets/images/pfp.jpg',
+                  width: 110,
+                  height: 110,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 5,),
+              Text(
+                'John Doe',
+                style: TextStyle(
+                    color: const Color.fromARGB(255, 58, 68, 72),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
+              const SizedBox(height: 5,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  'John Doe exists. John Doe builds. John Doe innovates. What’s next? Only time will tell...',
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 15,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const SizedBox(height: 20 ,), 
+
+              SizedBox(
+                height: MediaQuery.sizeOf(context).height * 0.40,
+                child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        MyEventTile(),
+                        const SizedBox(height: 20,)
+                      ],
+                    );
+                  },
+                  ),
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/widgets/event_tile.dart
+++ lib/widgets/event_tile.dart
@@ -1,38 +1,37 @@
 import 'package:flutter/material.dart';
 
-class EventTile extends StatelessWidget {
-  const EventTile({super.key});
+class MyEventTile extends StatelessWidget {
+  const MyEventTile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      elevation: 10,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 5),
-        child: ListTile(
-          leading: AspectRatio(
-            aspectRatio: 1.5,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: SizedBox(
-                width: 300,
-                height: 300,
-                child: Image.asset('assets/images/event.png', fit: BoxFit.cover,),
-              ),
-            ),
-          ),
-          title: Text('Event Name', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('30 November 2024, 11:00 PM',  style: TextStyle(fontWeight: FontWeight.normal, fontSize: 12, color: Color(0xFF929292))),
-              Text('13th Street, Park Avenue',  style: TextStyle(fontWeight: FontWeight.normal, fontSize: 12, color: Color(0xFF929292))),
-              Text('John Doe',  style: TextStyle(fontWeight: FontWeight.normal, fontSize: 12, color: Color(0xFF929292))),
-            ],
-          ),
-        ),
-      ),
-    );
+    return Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Material(
+                  elevation: 10,
+                  child: ListTile(
+                  
+                    leading: ClipRRect(
+                      borderRadius: BorderRadius.circular(5),
+                      child: Image.asset('assets/images/event.png', fit: BoxFit.cover,)
+                      ),
+                    title: Text(
+                      'Event Name', 
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold, 
+                        fontSize: 15,
+                      ),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('30 November 2024, 9:00 AM'),
+                        Text('13th Street, Park Avenue'),
+                        Text('John Doe'),
+                      ],
+                    ),
+                  ),
+                ),
+              );
   }
 }
