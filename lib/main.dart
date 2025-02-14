b1f66912b12f0cbd04db68a47b4ef7c89b1cacd0
zainab-frfr <135985226+zainab-frfr@users.noreply.github.com>
2025-02-14 18:45:43 +0500

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tutorial_1/profile_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,7 +11,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyProfilePage()
+      home: Scaffold()
     );
   }
 }
